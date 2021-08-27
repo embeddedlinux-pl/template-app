f63cd344c069e96d6219cf4b9813c60bc95ee376
Kuba Sejdak <kuba.sejdak@gmail.com>
2021-08-27 07:36:54 +0000

    Add basic layout.

---
+++ app/utils/include/utils/app/ApplicationLogger.hpp
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////////////
+///
+/// @file
+/// @author Kuba Sejdak
+/// @copyright BSD 2-Clause License
+///
+/// Copyright (c) 2021-2021, Kuba Sejdak <kuba.sejdak@gmail.com>
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions are met:
+///
+/// 1. Redistributions of source code must retain the above copyright notice, this
+///    list of conditions and the following disclaimer.
+///
+/// 2. Redistributions in binary form must reproduce the above copyright notice,
+///    this list of conditions and the following disclaimer in the documentation
+///    and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+/// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+/// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+/// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+/// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
+/////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/syslog_sink.h>
+#include <spdlog/spdlog.h>
+
+#include <string>
+
+namespace app {
+
+/// Application logger creator.
+/// Object of this type will be used internally by the logger to create logging sinks.
+struct ApplicationLoggerCreator {
+    /// Constructs and registers the module logger which logs to both stdout (console) and syslog.
+    /// @param name         Name of the logger (used as a tag in the log prefix).
+    /// @param level        Default logging level used by this logger.
+    void operator()(const std::string& name, spdlog::level::level_enum level)
+    {
+        auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        consoleSink->set_level(spdlog::level::trace);
+        consoleSink->set_pattern("[%n][%^%L%$][%T.%e] %v");
+
+        std::string ident = "template-app";
+        int syslogOptions = 0;
+        auto syslogSink = std::make_shared<spdlog::sinks::syslog_sink_mt>(ident, syslogOptions, LOG_PID, true);
+        syslogSink->set_level(spdlog::level::trace);
+        syslogSink->set_pattern("[%n][%^%L%$][%T.%e] %v");
+
+        auto logger = std::make_shared<spdlog::logger>(name, spdlog::sinks_init_list{consoleSink, syslogSink});
+        logger->set_level(level);
+        spdlog::register_logger(logger);
+    }
+};
+
+} // namespace app
